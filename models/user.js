4265336ad7e35b8c5b95da9c2e98413363ca1bbf
Obada-Fa <ubadafatta@gmail.com>
2025-06-03 10:57:22 +0200

    added db user schema

---
+++ models/user.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name:{type: String, required: true},
+    password: {type: String, required: true},
+    role: {type: Number, default: 0},
+    coins: {type: Number, default: 0},
+    upgradeStage: {type: Number, default: 1},
+});
