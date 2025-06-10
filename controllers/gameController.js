e740f84acc4e952d734c7d547e60ac52fab9bc38
Yosei10200 <yosei.saito@gmail.com>
2025-06-10 16:06:20 +0200

    Paar fixes

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -15,8 +15,6 @@ export const registerUser = async (req, res) => {
     }
     const salt = await bcrypt.genSalt(10)
     const hashedPassword = await bcrypt.hash(password, salt)
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
     const user = new User({ name, password: hashedPassword, role });
     await user.save();
     res.status(201).json({ message: "User created", user });
@@ -191,7 +189,6 @@ export const logOutUser = async (req, res) =>{
   }catch (error){
     res.status(500).json({message: error})
   }
-}
 };
 
 export const getUserCoins = async (req, res) => {
